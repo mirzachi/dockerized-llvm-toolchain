2d610f9d6e971dbb52e908349a7243f105638602
Mirza Avdic <mirza.avdic@tum.de>
2021-07-09 06:37:09 -0700

    install-llvm.sh common script updated

--- common/install-llvm.sh
+++ common/install-llvm.sh
@@ -15,14 +15,14 @@ set -eux
 needed_binaries=(lsb_release wget add-apt-repository)
 missing_binaries=()
 for binary in "${needed_binaries[@]}"; do
-    if ! which $binary &>/dev/null ; then
-        missing_binaries+=($binary)
-    fi
+  if ! which $binary &>/dev/null; then
+    missing_binaries+=($binary)
+  fi
 done
-if [[ ${#missing_binaries[@]} -gt 0 ]] ; then
-    echo "You are missing some tools this script requires: ${missing_binaries[@]}"
-    echo "(hint: apt install lsb-release wget software-properties-common)"
-    exit 4
+if [[ ${#missing_binaries[@]} -gt 0 ]]; then
+  echo "You are missing some tools this script requires: ${missing_binaries[@]}"
+  echo "(hint: apt install lsb-release wget software-properties-common)"
+  exit 4
 fi
 
 # set llvm version to install
@@ -33,8 +33,8 @@ VERSION=$(lsb_release -sr)
 DIST_VERSION="${DISTRO}_${VERSION}"
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root!"
-   exit 1
+  echo "This script must be run as root!"
+  exit 1
 fi
 
 declare -A LLVM_VERSION_PATTERNS
@@ -45,34 +45,54 @@ LLVM_VERSION_PATTERNS[12]="-12"
 LLVM_VERSION_PATTERNS[13]=""
 
 if [ ! ${LLVM_VERSION_PATTERNS[$LLVM_VERSION]+_} ]; then
-    echo "This script does not support LLVM version $LLVM_VERSION"
-    exit 3
+  echo "This script does not support LLVM version $LLVM_VERSION"
+  exit 3
 fi
 
 LLVM_VERSION_STRING=${LLVM_VERSION_PATTERNS[$LLVM_VERSION]}
 
 # find the right repository name for the distro and version
 case "$DIST_VERSION" in
-    Debian_9* )       REPO_NAME="deb http://apt.llvm.org/stretch/  llvm-toolchain-stretch$LLVM_VERSION_STRING main" ;;
-    Debian_10* )      REPO_NAME="deb http://apt.llvm.org/buster/   llvm-toolchain-buster$LLVM_VERSION_STRING  main" ;;
-    Debian_11* )      REPO_NAME="deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye$LLVM_VERSION_STRING  main" ;;
-    Debian_unstable ) REPO_NAME="deb http://apt.llvm.org/unstable/ llvm-toolchain$LLVM_VERSION_STRING         main" ;;
-    Debian_testing )  REPO_NAME="deb http://apt.llvm.org/unstable/ llvm-toolchain$LLVM_VERSION_STRING         main" ;;
-    Ubuntu_16.04 )    REPO_NAME="deb http://apt.llvm.org/xenial/   llvm-toolchain-xenial$LLVM_VERSION_STRING  main" ;;
-    Ubuntu_18.04 )    REPO_NAME="deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic$LLVM_VERSION_STRING  main" ;;
-    Ubuntu_18.10 )    REPO_NAME="deb http://apt.llvm.org/cosmic/   llvm-toolchain-cosmic$LLVM_VERSION_STRING  main" ;;
-    Ubuntu_19.04 )    REPO_NAME="deb http://apt.llvm.org/disco/    llvm-toolchain-disco$LLVM_VERSION_STRING   main" ;;
-    Ubuntu_19.10 )   REPO_NAME="deb http://apt.llvm.org/eoan/      llvm-toolchain-eoan$LLVM_VERSION_STRING    main" ;;
-    Ubuntu_20.04 )   REPO_NAME="deb http://apt.llvm.org/focal/     llvm-toolchain-focal$LLVM_VERSION_STRING   main" ;;
-    Ubuntu_20.10 )   REPO_NAME="deb http://apt.llvm.org/groovy/    llvm-toolchain-groovy$LLVM_VERSION_STRING  main" ;;
-    Ubuntu_21.04 )   REPO_NAME="deb http://apt.llvm.org/hirsute/   llvm-toolchain-hirsute$LLVM_VERSION_STRING main" ;;
-    * )
-        echo "Distribution '$DISTRO' in version '$VERSION' is not supported by this script (${DIST_VERSION})."
-        exit 2
+Debian_9*) REPO_NAME="deb http://apt.llvm.org/stretch/  llvm-toolchain-stretch$LLVM_VERSION_STRING main" ;;
+Debian_10*) REPO_NAME="deb http://apt.llvm.org/buster/   llvm-toolchain-buster$LLVM_VERSION_STRING  main" ;;
+Debian_11*) REPO_NAME="deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye$LLVM_VERSION_STRING  main" ;;
+Debian_unstable) REPO_NAME="deb http://apt.llvm.org/unstable/ llvm-toolchain$LLVM_VERSION_STRING         main" ;;
+Debian_testing) REPO_NAME="deb http://apt.llvm.org/unstable/ llvm-toolchain$LLVM_VERSION_STRING         main" ;;
+Ubuntu_16.04) REPO_NAME="deb http://apt.llvm.org/xenial/   llvm-toolchain-xenial$LLVM_VERSION_STRING  main" ;;
+Ubuntu_18.04) REPO_NAME="deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic$LLVM_VERSION_STRING  main" ;;
+Ubuntu_18.10) REPO_NAME="deb http://apt.llvm.org/cosmic/   llvm-toolchain-cosmic$LLVM_VERSION_STRING  main" ;;
+Ubuntu_19.04) REPO_NAME="deb http://apt.llvm.org/disco/    llvm-toolchain-disco$LLVM_VERSION_STRING   main" ;;
+Ubuntu_19.10) REPO_NAME="deb http://apt.llvm.org/eoan/      llvm-toolchain-eoan$LLVM_VERSION_STRING    main" ;;
+Ubuntu_20.04) REPO_NAME="deb http://apt.llvm.org/focal/     llvm-toolchain-focal$LLVM_VERSION_STRING   main" ;;
+Ubuntu_20.10) REPO_NAME="deb http://apt.llvm.org/groovy/    llvm-toolchain-groovy$LLVM_VERSION_STRING  main" ;;
+Ubuntu_21.04) REPO_NAME="deb http://apt.llvm.org/hirsute/   llvm-toolchain-hirsute$LLVM_VERSION_STRING main" ;;
+*)
+  echo "Distribution '$DISTRO' in version '$VERSION' is not supported by this script (${DIST_VERSION})."
+  exit 2
+  ;;
 esac
 
 # install everything
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 add-apt-repository "${REPO_NAME}"
 apt-get update
-apt-get install -y clang-$LLVM_VERSION lldb-$LLVM_VERSION lld-$LLVM_VERSION clangd-$LLVM_VERSION
+apt-get install clang-format-$LLVM_VERSION \
+  clang-tidy-$LLVM_VERSION \
+  clang-tools-$LLVM_VERSION \
+  clang-$LLVM_VERSION \
+  clangd-$LLVM_VERSION \
+  libc++-dev-$LLVM_VERSION \
+  libc++1-$LLVM_VERSION \
+  libc++abi-dev-$LLVM_VERSION \
+  libc++abi1-$LLVM_VERSION \
+  libclang-dev\-$LLVM_VERSION \
+  libclang1-$LLVM_VERSION \
+  liblldb-dev-$LLVM_VERSION \
+  libllvm-ocaml-dev-$LLVM_VERSION \
+  libomp-dev-$LLVM_VERSION \
+  libomp5-$LLVM_VERSION \
+  lld-$LLVM_VERSION \
+  lldb-$LLVM_VERSION \
+  llvm-dev-$LLVM_VERSION \
+  llvm-runtime-$LLVM_VERSION \
+  llvm python-clang
